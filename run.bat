893bd9fc811f91bc88452d228ac86a71fdc5ae7a
USERSAD\rgonzafa <rgonzafa@emeal.nttdata.com>
2025-07-25 13:06:57 +0100

    Actualizar run.bat para ejecutar main.py y agregar soporte de sonido en ticker_display.py

--- run.bat
+++ run.bat
@@ -7,4 +7,4 @@ set "SCRIPT_DIR=%~dp0"
 
 REM El comando 'start "TÃ­tulo" /B' inicia un programa sin crear una nueva ventana.
 REM Usamos la ruta completa al script de Python para asegurar que se ejecute correctamente.
-start "TARCA" /B pythonw.exe "%SCRIPT_DIR%TARCA.py"
+start "TARCA" /B pythonw.exe "%SCRIPT_DIR%main.py"
